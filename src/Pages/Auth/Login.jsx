5ceb8052b81fd414a96f265266c63e1568e4ea90
waelaltyeb21 <waelaltyeb2124@gmail.com>
2024-08-14 04:35:00 -0700

    Change From Dev To Pro

--- src/Pages/Auth/Login.jsx
+++ src/Pages/Auth/Login.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 const Login = () => {
   return (
     <section>
@@ -64,12 +65,13 @@ const Login = () => {
             </div>
 
             <div>
-              <button
+              <Link
+                to="/dashboard/homepage"
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Sign in
-              </button>
+              </Link>
             </div>
           </form>
 
